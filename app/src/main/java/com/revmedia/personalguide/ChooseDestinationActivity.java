0e2e113acf20213a784e4ef126f1de374414046b
marvinnn <marvin12@mhs.if.its.ac.id>
2016-01-01 09:43:32 +0700

    Choose Destination UI

---
+++ app/src/main/java/com/revmedia/personalguide/ChooseDestinationActivity.java
@@ -0,0 +1,13 @@
+package com.revmedia.personalguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseDestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_destination);
+    }
+}
--- app/src/main/java/com/revmedia/personalguide/MainActivity.java
+++ app/src/main/java/com/revmedia/personalguide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.revmedia.personalguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void mapClick(View view){
+        Intent intent = new Intent(this, ChooseDestinationActivity.class);
+        startActivity(intent);
     }
 }
